5107c60cefb3e45880c6247f4d9baad6e4db36e7
CaioFGCU <cfbotelho9922@eagle.fgcu.edu>
2019-11-03 15:22:41 -0500

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,36 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+import java.awt.event.ActionEvent;
+
+
+
+
+public class Controller {
+
+        @FXML
+        private Text welcomeText;
+
+        @FXML
+        private Label labelUsername;
+
+        @FXML
+        private Label labelPassword;
+
+        @FXML
+        private TextField inputUsername;
+
+        @FXML
+        private PasswordField inputPassword;
+
+        @FXML
+        void Buttonpressed(ActionEvent event) {
+
+        }
+
+}
